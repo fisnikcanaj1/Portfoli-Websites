b90ef79cfdc432eb8fd5f7cce28694fdc36d7b81
Your Name <fisnikcanaj2@gmail.com>
2017-09-03 08:49:34 +0200

    Header about work and footer section done

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
 
-  //
+		$('a[href^="#"]').on('click', function (e) {
+
+			var target = $($(this).attr('href'));
+
+			e.preventDefault();
+			console.log(target);
+			$('html, body').animate({
+				'scrollTop': target.offset().top
+			}, 300, 'swing');
+		});
 
 });
